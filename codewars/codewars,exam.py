7b95dc6718dc26089b12a70f00c1d1bfb4565121
zuka <157312286+zukadzm@users.noreply.github.com>
2024-08-23 05:22:14 -0700

    Add files via upload

---
+++ codewars/codewars,exam.py
@@ -0,0 +1,43 @@
+# 8 kyu
+# 1) 8kyu def get_real_floor(n):  
+#         if n <= 0:
+#             return n
+#         elif n < 13:
+#             return n - 1
+#         return n - 2
+            
+
+
+# 2) 8kyu
+# def duty_free(price, discount, holiday_cost):
+#     result_first = discount * 0.01
+#     result_second = price * result_first
+#     return holiday_cost // result_second
+
+
+# 7 kyu
+
+# 1) 7kyu
+# def last_survivor(letters, coords): 
+#     letters = list(letters)
+#     for coordd in coords:
+#         del (letters[coordd])
+#     return "".join(letters)
+
+
+# 6kyu
+
+# 1 6kyu 
+# def order(sentence):
+#     result = []
+#     sentence = sentence.split()
+#     for i in range(1, len(sentence) + 1):
+#         for element in sentence:
+#             if str(i) in element:
+#                 result.append(element)
+#     return " ".join(result)      
+
+
+
+# https://www.codewars.com/collections/exam?fbclid=IwY2xjawE1OfBleHRuA2FlbQIxMAABHWa9a_fkyOTIiRpmGFx3zYlrAXv2szaVNCclCDrXfIBAWVDDENcts_yU9Q_aem_ILi4O25tcmttz4Pc1eEOWQ
+# საბოლოოდ მე გავაკეთე 2,8 kyu, 1,7kyu 1,6kyu
